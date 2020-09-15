8c53ffdd9162bbca3b983b157126d5fd2029419d
Sean Ahn <sean.ahn@c3iot.com>
2020-09-14 20:55:26 -0700

    Cleaned up tests - even more

--- echo/src/test/scala/echo/EchoTest.scala
+++ echo/src/test/scala/echo/EchoTest.scala
@@ -5,11 +5,14 @@ import org.scalatest.matchers.should.Matchers._
 
 class EchoTest extends AnyFlatSpec {
   "Apply constructor" should "have been created" in {
-     Echo(greetings = "Yo", times = 2).echo("How are you!") shouldBe "Yo How are you! Yo How are you!"
-
      val echo1 = Echo(greetings = "Yo", times = 2)
      (echo1.greetings, echo1.times) shouldBe ("Yo", 2)
-     echo1.echoTo(msg = "How are you!")(target = "Tom") shouldBe "Yo Tom! How are you! Yo Tom! How are you!"
+  }
+
+  "Member method" should "work" in {
+     Echo(greetings = "Yo", times = 2).echo("How are you!") shouldBe "Yo How are you! Yo How are you!"
+
+     Echo(greetings = "Yo", times = 1).echoTo(msg = "How are you!")(target = "Tom") shouldBe "Yo Tom! How are you!"
   }
 
   "Module" should "delegate call to static" in {
